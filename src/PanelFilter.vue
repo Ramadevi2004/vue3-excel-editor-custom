ca1a28e3e05f414022b45af1b67f5ee062be7849
Ramadevi2004 <bourisettyvaralaramadevi@gmail.com>
2025-07-14 17:14:07 +0530

    addded into mark in panel filter

--- src/PanelFilter.vue
+++ src/PanelFilter.vue
@@ -2,15 +2,19 @@
   <div id="panelModal" v-show="show" class="panel-modal" @click="clickOutside" @keydown.exact.esc="hidePanel">
     <div ref="panelFilter" class="panel-body">
       <div class="panel-title">
-        <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="sort-amount-down" role="img"
-          xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"
-          class="svg-inline--fa fa-sort-amount-down fa-w-16 fa-1x">
-          <path fill="currentColor"
-            d="M304 416h-64a16 16 0 0 0-16 16v32a16 16 0 0 0 16 16h64a16 16 0 0 0 16-16v-32a16 16 0 0 0-16-16zm-128-64h-48V48a16 16 0 0 0-16-16H80a16 16 0 0 0-16 16v304H16c-14.19 0-21.37 17.24-11.29 27.31l80 96a16 16 0 0 0 22.62 0l80-96C197.35 369.26 190.22 352 176 352zm256-192H240a16 16 0 0 0-16 16v32a16 16 0 0 0 16 16h192a16 16 0 0 0 16-16v-32a16 16 0 0 0-16-16zm-64 128H240a16 16 0 0 0-16 16v32a16 16 0 0 0 16 16h128a16 16 0 0 0 16-16v-32a16 16 0 0 0-16-16zM496 32H240a16 16 0 0 0-16 16v32a16 16 0 0 0 16 16h256a16 16 0 0 0 16-16V48a16 16 0 0 0-16-16z"
-            class=""></path>
-        </svg>
-
-        <span v-html="localizedLabel.sortingAndFiltering" />
+        <div class="panel-title-left">
+          <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="sort-amount-down" role="img"
+            xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"
+            class="svg-inline--fa fa-sort-amount-down fa-w-16 fa-1x">
+            <path fill="currentColor"
+              d="M304 416h-64a16 16 0 0 0-16 16v32a16 16 0 0 0 16 16h64a16 16 0 0 0 16-16v-32a16 16 0 0 0-16-16zm-128-64h-48V48a16 16 0 0 0-16-16H80a16 16 0 0 0-16 16v304H16c-14.19 0-21.37 17.24-11.29 27.31l80 96a16 16 0 0 0 22.62 0l80-96C197.35 369.26 190.22 352 176 352zm256-192H240a16 16 0 0 0-16 16v32a16 16 0 0 0 16 16h192a16 16 0 0 0 16-16v-32a16 16 0 0 0-16-16zm-64 128H240a16 16 0 0 0-16 16v32a16 16 0 0 0 16 16h128a16 16 0 0 0 16-16v-32a16 16 0 0 0-16-16zM496 32H240a16 16 0 0 0-16 16v32a16 16 0 0 0 16 16h256a16 16 0 0 0 16-16V48a16 16 0 0 0-16-16z"
+              class=""></path>
+          </svg>
+          <span v-html="localizedLabel.sortingAndFiltering" />
+        </div>
+        <button class="panel-close-btn" @click="hidePanel" title="Close">
+          ×
+        </button>
       </div>
       <div class="panel-content">
         <div class="panel-action">
@@ -102,7 +106,7 @@
                     :indeterminate="isSelectAllIndeterminate"
                     @change="toggleSelectAll($event.target.checked)"
                   />
-                  <strong>Select All</strong>
+                  Select All
                 </div>
               </label>
             </div>
@@ -531,8 +535,33 @@ input:focus, button:focus {
   font-size: 1.25rem;
   line-height: 1.5rem;
   align-items: center;
-  justify-content: flex-start;
+  justify-content: space-between;
   border-bottom: 1px solid lightgray;
+}
+
+.panel-title-left {
+  display: flex;
+  align-items: center;
+}
+
+.panel-close-btn {
+  background: none;
+  border: none;
+  font-size: 1.4rem;
+  cursor: pointer;
+  color: #dc3545;
+  padding: 0;
+  width: 24px;
+  height: 24px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 3px;
+}
+
+.panel-close-btn:hover {
+  background-color: #f8d7da;
+  color: #721c24;
 }
 
 div.panel-title span, button.panel-button span {
@@ -695,7 +724,6 @@ div.panel-title span, button.panel-button span {
 .select-all-item {
   background-color: #f8f9fa;
   border-bottom: 2px solid #dee2e6 !important;
-  font-weight: bold;
 }
 
 .select-all-item:hover {
